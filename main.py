21d75e785807ddde02f605e05643f007df807646
Tatiana Grechishcheva <tatiana.grechishcheva@your.md>
2021-08-25 19:45:44 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+path = '/Users/tatianagrechishcheva/Documents/My Projects/Qdrant'
+with open(path+'/vectors.json') as jsonFile:
+    vectors = json.load(jsonFile)
+    jsonFile.close()
+
+collection_obj = {
+    "create_collection": {
+        "name": "clothes_collection",
+        "distance": "Cosine",
+        "vector_size": 1000
+    }
+}
+x = requests.post('http://localhost:6333/collections', json = collection_obj)
+
+img_ids = list(vectors.keys())
+for i in range(0,len(vectors)):
+    point_obj = {
+            "upsert_points": {
+                "points": [
+                    {
+                        "id": i,
+                        "payload": {},
+                        "vector": vectors[img_ids[i]]
+                    }
+                ]
+            }
+        }
+    x = requests.post('http://localhost:6333/collections/clothes_collection', json = point_obj)
